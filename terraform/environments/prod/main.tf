416d6d9dbe11da1ad667b2149e5733c7c8ef55a0
Yohei Okabayashi <yohei.kuro48@gmail.com>
2025-05-26 23:53:56 +0900

    Add module for enabling required APIs in project configuration

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -9,9 +9,23 @@ module "tfstate_bucket" {
   gcp_project_id = data.google_project.project.project_id
 }
 
+# enable the required APIs for the project
+module "project-services" {
+  source  = "terraform-google-modules/project-factory/google//modules/project_services"
+  version = "~> 18.0"
+
+  project_id = var.gcp_project_id
+
+  activate_apis = [
+    "iam.googleapis.com",
+    "iamcredentials.googleapis.com"
+  ]
+}
+
 # create workload identity pool for GitHub Actions
 module "gha_workload_identity" {
   source         = "../../modules/gha_workload_identity"
   gcp_project_id = data.google_project.project.project_id
   repositories   = var.repositories
 }
+
