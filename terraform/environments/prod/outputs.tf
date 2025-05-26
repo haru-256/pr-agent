1220dff2d2c7931fa0d772b8f66a2f1a8bf00053
Yohei Okabayashi <yohei.kuro48@gmail.com>
2025-05-26 22:23:01 +0900

    Refactor GitHub Actions workload identity outputs for clarity and consistency

--- terraform/environments/prod/outputs.tf
+++ terraform/environments/prod/outputs.tf
@@ -2,3 +2,17 @@ output "tfstate_bucket_id" {
   value       = module.tfstate_bucket.tfstate_gcs_bucket_id
   description = "The ID of the bucket used to store terraform state"
 }
+
+
+output "gha_workload_identity_provider_name" {
+  value       = module.gha_workload_identity.workload_identity_provider_name
+  description = "The mapping of service accounts for GitHub Actions workload identity"
+
+}
+
+output "gha_workload_identity_service_account_email" {
+  value = {
+    for repo, sa in module.gha_workload_identity.service_account_email : repo => sa
+  }
+  description = "The mapping of service accounts for GitHub Actions workload identity"
+}
--- terraform/modules/gha_workload_identity/outputs.tf
+++ terraform/modules/gha_workload_identity/outputs.tf
@@ -1,5 +1,11 @@
-output "workload_identity_pool_id" {
-  value       = module.gh_oidc
-  description = "The ID of each workload identity pool for "
+output "workload_identity_provider_name" {
+  value       = module.gh_oidc.provider_name
+  description = "The provider name of workload identity provider for GitHub Actions"
 }
 
+output "service_account_email" {
+  value = {
+    for repo, sa in google_service_account.gh : repo => sa.email
+  }
+  description = "The mapping of service accounts for GitHub Actions workload identity"
+}
